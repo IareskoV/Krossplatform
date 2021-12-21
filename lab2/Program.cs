25d56c64d3eca09a31ebc7a9a2d3549f1134bdc3
Vlad Iaresko <vladyaresko01@gmail.com>
2021-12-21 14:11:14 +0200

    remake lab 2

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -9,21 +9,12 @@ namespace lab2
     {
         static void Main(string[] args)
         {
-            int[,] array = new int[255, 255];
-            for (var i = 1; i < 255; i++)
-            {
-                array[i, i] = 2;
-            }
-            var startx = 0;
-            var starty = 0;
-            array = transform(array, startx, starty);
+            
 
-            Console.Write(array[22, 20]);
-
-            List<int> arr = new List<int>();
+            List<double> arr = new List<double>();
             var N = 0;
+            double p = 1;
             var tempS = System.IO.File.ReadAllText("input.txt").Split(' ', '\r', '\n', '\t');
-
 
 
             foreach (string element in tempS)
@@ -35,18 +26,24 @@ namespace lab2
                 else if (!string.IsNullOrEmpty(element))
                 {
                     Console.WriteLine(string.IsNullOrEmpty(element));
-                    arr.Add(Convert.ToInt32(element));
+                    string temp = element.Replace('.', ',');
+                    arr.Add(Convert.ToDouble(temp));
                 }
             }
 
-            var result = array[Convert.ToInt32(tempS[0]), Convert.ToInt32(tempS[1])];
+            for (int z = 0; z < N; z++)
+            {
+                p = p * arr[z] + (1 - p) * (1 - arr[z]);
+            }
+
+            string result = Convert.ToString(p);
 
 
 
 
 
 
-            System.IO.File.WriteAllText("output.txt", string.Join("player - ", result));
+            System.IO.File.WriteAllText("output.txt", result);
 
 
             Console.Write(arr);
@@ -54,47 +51,6 @@ namespace lab2
             //File.WriteAllText("output.txt", result);
         }
 
-        static int[,] transform(int[,] test, int startx, int starty)
-        {
-            if (test[startx,starty] == 0)
-            {
-                for (var i = 1; i < 35 - startx; i++)
-                {
-                    test[i + startx,starty] = 2;
-                }
-                for (var i = 1; i < 35 - starty; i++)
-                {
-                    test[startx,i + starty] = 2;
-
-                }
-                if (startx == 254)
-                {
-                    if (starty == 254)
-                    {
-                        return test;
-                    }
-                    else
-                    {
-                        startx = 0;
-                        starty += 1;
-                        Console.WriteLine('2');
-                        transform(test, startx, starty);
-                    }
-                    
-                }
-                else
-                {
-
-                    transform(test, startx + 1,starty);
-                }
-
-                return test;
-            }
-            else
-            {
-                return test;
-            }
-
-        }
-        }
+        
+    }
 }
