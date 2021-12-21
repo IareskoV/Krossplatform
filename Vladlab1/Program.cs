65d73b8cfce02453392c67d99fb27d045912c5ba
Vlad Iaresko <vladyaresko01@gmail.com>
2021-12-21 13:32:21 +0200

    remake lab1

--- Vladlab1/Program.cs
+++ Vladlab1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 namespace Vladlab1
 {
     class Program
@@ -16,16 +15,13 @@ namespace Vladlab1
 
             foreach(string element in tempS)
             {
-                if (element == tempS[0])
-                {
-                    N = Convert.ToInt32(element);
-                }
-                else if (!string.IsNullOrEmpty(element))
+                if (!string.IsNullOrEmpty(element))
                 {
                     Console.WriteLine(string.IsNullOrEmpty(element));
                     arr.Add(Convert.ToInt32(element));
                 }
             }
+            Console.WriteLine(arr);
 
             var sum = 0;
             for(int i = 0; i<arr.Count ;i++ )
@@ -34,11 +30,12 @@ namespace Vladlab1
             }
 
             sum = sum - solve(arr);
+            string ansv = getMin(arr[0], arr[1]) + " " + getMax(arr[0], arr[1]);
 
 
 
 
-            System.IO.File.WriteAllText("output.txt", string.Join(" ", sum));
+            System.IO.File.WriteAllText("output.txt", ansv);
 
 
             Console.Write(arr);
@@ -63,6 +60,51 @@ namespace Vladlab1
                 }
             }
             return temp + temp1 + temp2;
+        }
+
+        static int getMax( int s, int k)
+        {
+            int maxn = Convert.ToInt32(Math.Pow(10, k))-1;
+            int ans;
+            for(int j =maxn; j >=1;j--)
+            {
+                int n, sum = 0, m;
+                n = j;
+                while (n > 0)
+                {
+                    m = n % 10;
+                    sum = sum + m;
+                    n = n / 10;
+                }
+                if(sum == s)
+                {
+                    return j;
+                } 
+
+            }
+            return ans = 0;
+        }
+        static int getMin(int s, int k)
+        {
+            int maxn = Convert.ToInt32(Math.Pow(10, k)) - 1;
+            int ans;
+            for (int j = Convert.ToInt32(Math.Pow(10, k-1)); j <= maxn; j++)
+            {
+                int n, sum = 0, m;
+                n = j;
+                while (n > 0)
+                {
+                    m = n % 10;
+                    sum = sum + m;
+                    n = n / 10;
+                }
+                if (sum == s)
+                {
+                    return j;
+                }
+
+            }
+            return ans = 0;
         }
 
     }
